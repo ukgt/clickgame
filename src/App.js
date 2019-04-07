02a5d498504b565febbce21b823b0b7f4942c76b
ukgt <ukgt2019@gmail.com>
2019-04-06 21:55:22 -0400

    all items rendering

--- src/App.js
+++ src/App.js
@@ -9,14 +9,23 @@ class App extends Component {
   state = {
     images,
     score: 0,
-    totalscore: 0
+    totalscore: 0,
   };
-
- 
+  
+  
   //   need logic here -
- 
+// this.handleClick = this.handleClick.bind(this)
+//   handleClick(id) {
+//     this.setState(prevState => ({
+//       ...prevState,
+//       image: prevState.images.map(image => ({
+//         ...image,
+//         like: image.id === id ? image.like - 1 : image.like
+//      }))
+//     }))
+//   }
 
-handleClick = () => {
+handleClick = (id) => {
 this.setState(prevState => {
   return { score: prevState.score + 1}
 })
@@ -28,14 +37,16 @@ this.setState(prevState => {
       <>
         <Header  score={this.state.score} totalscore={this.state.totalscore}/>
         <Wrapper>
-          {this.state.images.map(pineapple => (
+          {this.state.images.map(image => (
             <Tiles
-              id={pineapple.id}
-              key={pineapple.id}
-              name={pineapple.name}
-              image={pineapple.image}
-            />
-          ))}
+              id={image.id}
+              key={image.id}
+              name={image.name}
+              image={image.image}
+             
+              
+              />
+              ))}
         </Wrapper>
       </>
     );
--- src/components/Tiles/Tiles.js
+++ src/components/Tiles/Tiles.js
@@ -2,15 +2,23 @@ import React from "react";
 import "./Tiles.css";
 
 
-const Tiles = props => (
-  <div className="card">
-    <div className="img-container">
-      <img alt={props.id} src={props.image} />
+// const Tiles = props => (
+  
+// ); 
 
-      <span handleClick={() => props.handleClick(props.id)} className="images" />
+ function Tiles(props) {
+    return <div className="row">{props.children}
+   
+    <div className="col-mid-3" />
+
+    <div className="card">
+      <div className="img-container">
+        <img alt={props.id} src={props.image} />
+
+        <div onClick={() => props.handleClick(props.id)} className="images" />
+      </div>
     </div>
   </div>
-); 
-
-
+  
+  };
 export default Tiles;
